a9ffacdccdd363cb64b3ef112940039700a67f7b
Peter Varkoly <varkoly@suse.com>
2016-07-01 17:37:22 +0200

    Increase Version.

--- maketar.sh
+++ maketar.sh
@@ -3,11 +3,13 @@
 find . -name '*.kdev4' -delete
 
 VERSION=`cat VERSION`
-rsync -aV src/ sap-installation-wizard-$VERSION/
-cp inifiles/*  sap-installation-wizard-$VERSION/includes/
-asks/create_products_xml.sh sap-installation-wizard-$VERSION
+rsync -av src/ sap-installation-wizard-$VERSION/
+cp inifiles/*  sap-installation-wizard-$VERSION/include/
+cd asks
+./create_products_xml.sh sap-installation-wizard-$VERSION
+cd ..
 
-tar chfvj "sap-installation-wizard-${VERSION}.tar.bz2" sap-installation-wizard-$VERSION
+tar chfj "sap-installation-wizard-${VERSION}.tar.bz2" sap-installation-wizard-$VERSION
 
 rm -rf sap-installation-wizard-$VERSION
 
