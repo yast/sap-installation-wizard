0ef5675600fe9c8c5a20909192510445e24bc6d7
Peter Varkoly <varkoly@suse.de>
2016-07-14 16:57:30 +0200

    Place Icons for sap-installation-wizard and windows_cheat_sheet on the desktop of root.

---
+++ src/misc/sap-profile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+xdg-desktop-icon install /usr/share/applications/YaST2/sap-windows_cheat_sheet.desktop
+xdg-desktop-icon install /usr/share/applications/YaST2/sap-installation-wizard.desktop
+
