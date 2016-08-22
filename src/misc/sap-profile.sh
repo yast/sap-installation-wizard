295f70c79948c9701c372027dce408a01a68b5ca
Peter Varkoly <varkoly@suse.com>
2016-08-22 20:28:26 +0200

    FATE#321142 Add SUSE Connect icon on desktop

--- src/misc/sap-profile.sh
+++ src/misc/sap-profile.sh
@@ -3,4 +3,5 @@
 xdg-desktop-icon install /usr/share/applications/YaST2/sap-windows_cheat_sheet.desktop
 xdg-desktop-icon install /usr/share/applications/YaST2/sap-installation-wizard.desktop
 xdg-desktop-icon install --novendor /usr/share/applications/YaST2/customer_center.desktop
+xdg-desktop-icon install --novendor /usr/share/applications/YaST2/suse-connect-program.desktop
 
