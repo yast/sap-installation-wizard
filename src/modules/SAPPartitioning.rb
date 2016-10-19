b5534718ffdf8d24ec4e11f788c87ce65c0e1fe6
zkalmar <zsolt.kalmar@suse.com>
2016-10-19 11:23:14 +0200

    Update SAPPartitioning.rb

--- src/modules/SAPPartitioning.rb
+++ src/modules/SAPPartitioning.rb
@@ -47,7 +47,7 @@ module Yast
               manufacturer
             )
             partXML = @partXMLPath + "/hana_partitioning.xml"
-            #TODO Do we hav to warn if generic partitioning happeds?
+            #TODO Do we hav to warn if generic partitioning happens?
           else
             # For comapitibility keep specific disk layout for Dell legacy models, but for new models use generic layout
             if !Builtins.contains(
