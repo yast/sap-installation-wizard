d5c2c585df9edcc972cd64def4ec90559f79150d
zkalmar <zsolt.kalmar@suse.com>
2016-11-24 14:27:23 +0100

    Update description.

--- src/include/trex_inst.sh
+++ src/include/trex_inst.sh
@@ -2,7 +2,7 @@
 
 # trex_inst.sh - is a script used to install SAP TREX
 #
-# Copyright (c) 2016 SUSE GmbH 
+# Copyright (c) 2016 SUSE Linux GmbH 
 #
 # This program is free software; you can redistribute it and/or 
 # modify it under the terms of the GNU General Public License as 
