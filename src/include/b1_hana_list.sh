39eafbe1febfb319edc3c8ccb57be6c5a052df60
Peter Varkoly <varkoly@suse.de>
2017-10-26 18:20:45 +0200

    B1 earns the SID and instance number parameters from HANA. Either it was saved into the product installations directory or the b1_hana_list.sh script evaluates the available HANA databases.

--- src/include/b1_hana_list.sh
+++ src/include/b1_hana_list.sh
@@ -18,17 +18,18 @@
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 # get a list of installed HANA DBs
-    all_sid_arr=(`ls -1 /hana/data/`)
+all_sid_arr=(`ls -1 /hana/data/`)
 
 # checking which one is running
-    for i in "${all_sid_arr[@]}"
-    do
-        ps aux | grep  -v grep | grep hdbindexserver | grep ${i,,} > /dev/null 2>&1
-        #echo $?
-        if [ $? -eq 0 ]; then
+echo -n '<selection config:type="list">'
+for i in "${all_sid_arr[@]}"
+do
+    ps aux | grep  -v grep | grep hdbindexserver | grep ${i,,} > /dev/null 2>&1
+    #echo $?
+    if [ $? -eq 0 ]; then
         inst_nr=`ls -1 /usr/sap/$i/ | grep HDB | cut -c 4-`
-        avl_sid_arr+=($i:$inst_nr)
-        fi
-    done
+        echo -n "<entry><value>$i:$inst_nr</value><label>$i:$inst_nr</label></entry>"
+    fi
+done
 #return the results
-echo $avl_sid_arr
+echo -n "</selection>"
