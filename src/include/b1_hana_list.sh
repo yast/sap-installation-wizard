7abae9ce30474794677940aa2a61eb65038d466f
zkalmar <zsolt.kalmar@suse.com>
2017-10-26 16:50:42 +0200

    Listing available HANA DBs for SAP B1

---
+++ src/include/b1_hana_list.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# b1_hana_inst.sh - is a script used to list available HANA DBs for SAP Business One installations.
+#   SAP Business One, version for SAP HANA
+#   SAP Business One analytics powered by SAP HANA
+#
+# Copyright (c) 2013 SAP AG
+#
+# This program is free software; you can redistribute it and/or 
+# modify it under the terms of the GNU General Public License as 
+# published by the Free Software Foundation only version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+# for more details.
+#
+# You should have received a copy of the GNU General Public License 
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+# get a list of installed HANA DBs
+    all_sid_arr=(`ls -1 /hana/data/`)
+
+# checking which one is running
+    for i in "${all_sid_arr[@]}"
+    do
+        ps aux | grep  -v grep | grep hdbindexserver | grep ${i,,} > /dev/null 2>&1
+        #echo $?
+        if [ $? -eq 0 ]; then
+        inst_nr=`ls -1 /usr/sap/$i/ | grep HDB | cut -c 4-`
+        avl_sid_arr+=($i:$inst_nr)
+        fi
+    done
+#return the results
+echo $avl_sid_arr
