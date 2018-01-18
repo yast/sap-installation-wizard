98e805c3149d0ef8466ee642e454f5be072283bd
Peter Varkoly <varkoly@suse.de>
2018-01-18 10:58:00 +0100

    bsc#1069572 - Build348.1 | SAP Desktop icons are not shown properly

--- src/misc/sap-profile.sh
+++ src/misc/sap-profile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [ -e /usr/bin/xdg-desktop-icon -a ! -e $HOME/.sap-icons ]; then 
+	gsettings set org.gnome.desktop.background show-desktop-icons true
 	xdg-desktop-icon install /usr/share/applications/YaST2/sap-windows_cheat_sheet.desktop
 	xdg-desktop-icon install /usr/share/applications/YaST2/sap-installation-wizard.desktop
 	xdg-desktop-icon install --novendor /usr/share/applications/YaST2/customer_center.desktop
