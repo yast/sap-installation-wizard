6d49655730cac8ee119bdf5e894667c10982eef3
Peter Varkoly <varkoly@suse.com>
2018-01-26 18:12:34 +0100

    1069572 - Build348.1 | SAP Desktop icons are not shown properly Build 392.1 | Root Desktop mit gnome-shell und fehlenden SAP-Icons

--- src/misc/sap-profile.sh
+++ src/misc/sap-profile.sh
@@ -6,5 +6,7 @@ if [ -e /usr/bin/xdg-desktop-icon -a ! -e $HOME/.sap-icons ]; then
 	xdg-desktop-icon install /usr/share/applications/YaST2/sap-installation-wizard.desktop
 	xdg-desktop-icon install --novendor /usr/share/applications/YaST2/customer_center.desktop
 	xdg-desktop-icon install --novendor /usr/share/applications/YaST2/suse-connect-program.desktop
+	gsettings set org.gnome.desktop.background show-desktop-icons true
+	chmod 755 $HOME/Desktop/*
 	touch $HOME/.sap-icons
 fi
