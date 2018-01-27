0e6e2ef43af543355b65961e9d1b38ec744dc253
Peter Varkoly <varkoly@suse.com>
2018-01-27 11:48:55 +0100

    1071696 - [Build 376.1] Clicking the sap-windows-cheat-sheet on YaST opens the SAP Installation Wizard 1069572
    Build348.1 | SAP Desktop icons are not shown properly
    Build 392.1 | Root Desktop using gnome-shell and missing SAP icons

--- src/misc/sap-profile.sh
+++ src/misc/sap-profile.sh
@@ -2,11 +2,12 @@
 
 if [ -e /usr/bin/xdg-desktop-icon -a ! -e $HOME/.sap-icons ]; then 
 	gsettings set org.gnome.desktop.background show-desktop-icons true
-	xdg-desktop-icon install /usr/share/applications/YaST2/sap-windows_cheat_sheet.desktop
+	xdg-desktop-icon install /usr/share/applications/sap-windows_cheat_sheet.desktop
 	xdg-desktop-icon install /usr/share/applications/YaST2/sap-installation-wizard.desktop
 	xdg-desktop-icon install --novendor /usr/share/applications/YaST2/customer_center.desktop
 	xdg-desktop-icon install --novendor /usr/share/applications/YaST2/suse-connect-program.desktop
+	chmod 755 /root/Desktop/*
+	echo "Trusted=true" >> /root/Desktop/customer_center.desktop
 	gsettings set org.gnome.desktop.background show-desktop-icons true
-	chmod 755 $HOME/Desktop/*
 	touch $HOME/.sap-icons
 fi
