824319025c8ae5177a0c1e9e8cec91d885d55bcc
Peter Varkoly <varkoly@suse.de>
2018-01-29 11:29:56 +0100

    Revent changes for desktop icons.

--- src/misc/sap-profile.sh
+++ src/misc/sap-profile.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ -e /usr/bin/xdg-desktop-icon -a ! -e $HOME/.sap-icons ]; then 
-	gsettings set org.gnome.desktop.background show-desktop-icons true
+	#gsettings set org.gnome.desktop.background show-desktop-icons true
 	xdg-desktop-icon install /usr/share/applications/sap-windows_cheat_sheet.desktop
 	xdg-desktop-icon install /usr/share/applications/YaST2/sap-installation-wizard.desktop
 	xdg-desktop-icon install --novendor /usr/share/applications/YaST2/customer_center.desktop
 	xdg-desktop-icon install --novendor /usr/share/applications/YaST2/suse-connect-program.desktop
-	chmod 755 /root/Desktop/*
-	echo "Trusted=true" >> /root/Desktop/customer_center.desktop
-	gsettings set org.gnome.desktop.background show-desktop-icons true
+	#chmod 755 /root/Desktop/*
+	#echo "Trusted=true" >> /root/Desktop/customer_center.desktop
+	#gsettings set org.gnome.desktop.background show-desktop-icons true
 	touch $HOME/.sap-icons
 fi
