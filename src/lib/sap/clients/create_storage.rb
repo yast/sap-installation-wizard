088f49ecfcdc372383c86a80941afbf5c636032e
Imobach González Sosa <igonzalezsosa@suse.com>
2018-04-25 16:25:58 +0100

    Show storage proposal relevant issues

--- src/lib/sap/clients/create_storage.rb
+++ src/lib/sap/clients/create_storage.rb
@@ -22,6 +22,8 @@
 require "sap/storage_proposal"
 require "y2storage"
 require "autoinstall/dialogs/disk_selector"
+require "autoinstall/dialogs/question"
+require "autoinstall/storage_proposal_issues_presenter"
 
 Yast.import "XML"
 Yast.import "Wizard"
@@ -74,6 +76,7 @@ module Y2Sap
           Yast::Popup.Error("Unable to create needed devices")
           return false
         end
+        return false unless check_issues(proposal.issues_list) == :ok
         proposal.save
         Y2Storage::StorageManager.instance.commit
         true
@@ -153,6 +156,32 @@ module Y2Sap
         end
 
         !existing.empty?
+      end
+
+      # @return Array<Class> List of relevant issues
+      RELEVANT_ISSUES_CLASSES = [
+        Y2Storage::AutoinstIssues::NoDiskSpace,
+        Y2Storage::AutoinstIssues::ShrinkedPlannedDevices,
+        Y2Storage::AutoinstIssues::Exception
+      ].freeze
+
+      # Check proposal issues
+      #
+      # Only a few issues are considered. The reason is that many of them are not
+      # applicable in this case. For instance, there is no point in checking if there
+      # is a / or a /boot partition in the proposal.
+      #
+      # If some relevant issue was found, a dialog will be displayed.
+      #
+      # @return [:ok,:abort]
+      def check_issues(issues_list)
+        relevant_issues = issues_list.select { |i| RELEVANT_ISSUES_CLASSES.include?(i.class) }
+        return :ok if relevant_issues.empty?
+        buttons_set = relevant_issues.any?(&:fatal?) ? :abort : :question
+        presenter = Y2Autoinstallation::StorageProposalIssuesPresenter.new(relevant_issues)
+        Y2Autoinstallation::Dialogs::Question.new(
+          presenter.to_html, timeout: 0, buttons_set: buttons_set
+        ).run
       end
     end
   end
