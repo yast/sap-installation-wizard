1c86fa40fdef7982a2fc338d26ab966d311fa179
Peter Varkoly <varkoly@suse.com>
2019-03-04 18:35:01 +0100

    Reduce warnings in validation.

--- src/lib/y2sap/products/variables.rb
+++ src/lib/y2sap/products/variables.rb
@@ -22,29 +22,28 @@
 module Y2Sap
   # Module for the global variables in the Y2SAP::Products class
   module ProductsVariables
-
     # Initialize the global variables
     def init_variables
       @dialog_text = {}
       @dialog_text = {
         nw_inst_type: {
           help:  _("<p>Choose SAP product installation and back-end database.</p>") +
-            '<p><b>' + _("SAP Standard System") + '</p></b>' +
+            "<p><b>" + _("SAP Standard System") + "</p></b>" +
             _("<p>Installation of a SAP NetWeaver system with all servers on the same host.</p>") +
-            '<p><b>' + _("SAP Standalone Engines") + '</p></b>' +
+            "<p><b>" + _("SAP Standalone Engines") + "</p></b>" +
             _("<p>Standalone engines are SAP Trex, SAP Gateway, and Web Dispatcher.</p>") +
-            '<p><b>' + _("Distributed System") + '</p></b>' +
+            "<p><b>" + _("Distributed System") + "</p></b>" +
             _("Installation of SAP NetWeaver with the servers distributed on separate hosts.</p>") +
-            '<p><b>' + _("High-Availability System") + '</p></b>' +
+            "<p><b>" + _("High-Availability System") + "</p></b>" +
             _("Installation of SAP NetWeaver in a high-availability setup.</p>") +
-            '<p><b>' + _("System Rename") + '</p></b>' +
+            "<p><b>" + _("System Rename") + "</p></b>" +
             _("Change the SAP system ID, database ID, instance number, or host name of a SAP system.</p>"),
           name: _("Choose the Installation Type!")
-          },
+        },
         nw_select_product: {
           help: _("<p>Please choose the SAP product you wish to install.</p>"),
           name: _("Choose a Product")
-          }
+        }
       }
       
       # @product_list contains a list of hashes of the parameter of the products which can be installed
@@ -55,23 +54,23 @@ module Y2Sap
       @product_list << {
         name:         "HANA",
         id:           "HANA",
-        ay_xml:       config_value("HANA","ay_xml"),
-        partitioning: config_value("HANA","partitioning"),
-        script_name:  config_value("HANA","script_name")
+        ay_xml:       config_value("HANA", "ay_xml"),
+        partitioning: config_value("HANA", "partitioning"),
+        script_name:  config_value("HANA", "script_name")
       }
       @product_list << {
         name:         "B1",
         id:           "B1",
-        ay_xml:       config_value("B1","ay_xml"),
-        partitioning: config_value("B1","partitioning"),
-        script_name:  config_value("B1","script_name")
+        ay_xml:       config_value("B1", "ay_xml"),
+        partitioning: config_value("B1", "partitioning"),
+        script_name:  config_value("B1", "script_name")
       }
       @product_list << {
         name:         "TREX",
         id:           "TREX",
-        ay_xml:       config_value("TREX","ay_xml"),
-        partitioning: config_value("TREX","partitioning"),
-        script_name:  config_value("TREX","script_name")
+        ay_xml:       config_value("TREX", "ay_xml"),
+        partitioning: config_value("TREX", "partitioning"),
+        script_name:  config_value("TREX", "script_name")
       }
       
       @DB            = ""
@@ -79,7 +78,7 @@ module Y2Sap
       @PRODUCT_ID    = ""
       @PRODUCT_NAME  = ""
       @product_map   = {}
-      @products_to_install = [];
+      @products_to_install = []
 
     end
   end
