34279b1a92c4abf06ed57018217b2ff46134c7b8
Peter Varkoly <varkoly@suse.com>
2019-07-30 15:22:34 +0200

    Add new partner. Make searching the right partitioner generic.

--- src/modules/SAPPartitioning.rb
+++ src/modules/SAPPartitioning.rb
@@ -30,59 +30,17 @@ module Yast
       model = Ops.get(hwinfo, 1, "") # "PowerEdge R620", "PowerEdge R910"
 
       Builtins.foreach(productPartitioningList) do |productPartitioning|
-        # For HANA we have hardware-dependent partitioning and if we don't know the manufacturer
-        # show warnings
-        partXML=@partXMLPath + '/' + productPartitioning + ".xml"
-        if productPartitioning == "hana_partitioning"
-          if !Builtins.contains(
-              [
-                "LENOVO",
-                "FUJITSU",
-                "IBM",
-                "HP",
-                "HPE",
-                "Dell Inc.",
-                "Huawei Technologies Co., Ltd.",
-                "Huawei"
-              ],
-              manufacturer
-            )
-            partXML = @partXMLPath + "/hana_partitioning.xml"
-            #TODO Do we have to warn if generic partitioning happens?
-            if productList.include?('B1')
-		if ! Popup.YesNoHeadline(_("Your System is not certified for SAP Business One on HANA."),
-			_("It is not guaranteed that your system will work properly. Do you want to continue the installation?"))
-		    return "abort"
-		end
-	    end
-
-          else
-            # For comapitibility keep specific disk layout for Dell legacy models, but for new models use generic layout
-            if !Builtins.contains(
-                  [
-                    "PowerEdge FC630",
-                    "PowerEdge M630",
-                    "PowerEdge R620",
-                    "PowerEdge R630",
-                    "PowerEdge R670",
-                    "PowerEdge R730xd",
-                    "PowerEdge R730",
-                    "PowerEdge R910",
-                    "PowerEdge R920",
-                    "PowerEdge RT30",
-                    "PowerEdge T620",
-                    "PowerEdge T630"
-                    ],
-                  model
-              )
-              partXML = @partXMLPath + '/' + productPartitioning + "_" + manufacturer + "_generic.xml"
-            else
-              partXML = @partXMLPath + '/' + productPartitioning + "_" + manufacturer + "_" + model + ".xml"
-            end
-          end
-        end #END HANA CASE
-        ret = WFM.CallFunction( "sap_create_storage", [ partXML ])
-        Builtins.y2milestone("sap_create_storage returned: %1",ret)
+        # This is a generic way for all SAP products and hardware
+        # Now it is possible to create product manufactutrer and model based partitioning files.
+        partXML = @partXMLPath + '/' + productPartitioning + "_" + manufacturer + "_" + model + ".xml"
+        if ! File.exist?(partXML)
+            partXML = @partXMLPath + '/' + productPartitioning + "_" + manufacturer + "_generic.xml"
+           if ! File.exist?(partXML)
+                partXML=@partXMLPath + '/' + productPartitioning + ".xml"
+           end
+        end
+        ret = WFM.CallFunction( "sap_create_storage_ng", [ partXML ])
+        Builtins.y2milestone("sap_create_storage_ng returned: %1",ret)
 	if( ret == "abort" )
 	    return "abort"
 	end
