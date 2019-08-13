0bb86e3a141542c9e591bdaa57e1ae4ab7e0a67b
Peter Varkoly <varkoly@suse.com>
2019-08-13 15:40:48 +0200

    bsc#1125865 ayast_setup restarts all services and kills the GUI Now we provides 3 HANA/B1 screnarios: 1. Installing HANA + B1 directly one after the other. 2. Installing only HANA 3. Installing B1 if already a HANA server is installed.

--- src/include/b1_hana_list.sh
+++ src/include/b1_hana_list.sh
@@ -18,18 +18,43 @@
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 # get a list of installed HANA DBs
-all_sid_arr=(`ls -1 /hana/data/`)
+SID='<selection config:type="list">'
+MASTERPW=""
+TIMEOUT=0
+if [ -d /hana/data ]
+then
+	all_sid_arr=(`ls -1 /hana/data/`)
 
-# checking which one is running
-echo -n '<selection config:type="list">'
-for i in "${all_sid_arr[@]}"
-do
-    ps aux | grep  -v grep | grep hdbindexserver | grep ${i,,} > /dev/null 2>&1
-    #echo $?
-    if [ $? -eq 0 ]; then
-        inst_nr=`ls -1 /usr/sap/$i/ | grep HDB | cut -c 4-`
-        echo -n "<entry><value>$i:$inst_nr</value><label>$i:$inst_nr</label></entry>"
-    fi
-done
-#return the results
-echo -n "</selection>"
+	# checking which one is running
+	for i in "${all_sid_arr[@]}"
+	do
+	    ps aux | grep  -v grep | grep hdbindexserver | grep ${i,,} > /dev/null 2>&1
+	    if [ $? -eq 0 ]; then
+		inst_nr=`ls -1 /usr/sap/$i/ | grep HDB | cut -c 4-`
+		SID="${SID}<entry><value>$i:$inst_nr</value><label>$i:$inst_nr</label></entry>"
+		break
+	    fi
+	done
+	#return the results
+else
+	. /etc/sysconfig/sap-installation-wizard
+	for i in $INSTDIR/*
+	do
+		if [ -e $i/product.data ]; then
+			grep PRODUCT_ID $i/product.data | grep -q HANA
+			if [ $? = 0 ]; then
+				inst_nr=$( cat $i/ay_q_sapinstnr )
+				sid=$( cat $i/ay_q_sid )
+				SID="${SID}<entry><value>$sid:$inst_nr</value><label>$sid:$inst_nr</label></entry>"
+				MASTERPW=$( cat $i/ay_q_masterPwd )
+				TIMEOUT=1
+				break
+			fi
+		fi
+	done
+fi
+SID="${SID}</selection>"
+sed "s#<default>___SAPSID___</default>#${SID}#g"        /usr/share/YaST2/include/sap-installation-wizard/B1.templ.xml > /usr/share/YaST2/include/sap-installation-wizard/B1.xml
+sed "s#___MASTERPW___#${MASTERPW}#g" -i /usr/share/YaST2/include/sap-installation-wizard/B1.xml
+sed "s#___TIMEOUT___#${TIMEOUT}#g"   -i /usr/share/YaST2/include/sap-installation-wizard/B1.xml
+
