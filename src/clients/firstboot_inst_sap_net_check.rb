3ad89802131d69446fa5b72dd392b68499803b80
Peter Varkoly <varkoly@suse.com>
2019-10-22 17:33:58 +0200

    bsc#1153429
    [Build 22.4] Button "Return to Network Setup" when FQHN is not defined returns to "Customer Center" screen

---
+++ src/clients/firstboot_inst_sap_net_check.rb
@@ -0,0 +1,49 @@
+# encoding: utf-8
+
+module Yast
+  class FirstbootInstSapNetCheckClient < Client
+    def main
+      Yast.import "UI"
+      Yast.import "Wizard"
+      Yast.import "Popup"
+      Yast.import "Service"
+
+      # MAIN
+
+      textdomain "sap-installation-wizard"
+
+      @contents  = nil
+      @help_text = ""
+      @closeMe   = false
+
+      if !Wizard.IsWizardDialog
+         Wizard.CreateDialog
+         @closeMe = true
+      end
+
+      Service.Restart("network")
+
+      # Check if hostname -f is set
+      @out = Convert.to_map(
+        SCR.Execute(path(".target.bash_output"), "hostname -f")
+      )
+      @hostname = Ops.get_string(@out, "stdout", "")
+      if @hostname == ""
+        if( Popup.AnyQuestion(_("The fully qualified hostname (FQHN) could not be detected."),
+                              _("Do you want to return to network setup or abort the SAP product installation and start the installed system?"),
+                              _("Return to Network Setup"),
+                              _("Abort"),
+                              :focus_yes
+                              ))
+            return :back
+         else
+            return :next
+         end
+      end
+      Wizard.CloseDialog() if @closeMe
+      return :next
+    end
+  end
+end
+
+Yast::FirstbootInstSapNetCheckClient.new.main
