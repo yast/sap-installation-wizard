228cb95a9089378d01a4fa8f66a96de77a6b793b
Peter Varkoly <varkoly@suse.com>
2019-10-28 14:02:35 +0100

    1153284
    SAP B1 image installer calculates hana partitions using GiB and not GB

--- src/clients/sap_create_storage.rb
+++ src/clients/sap_create_storage.rb
@@ -364,14 +364,22 @@ module Yast
     # Function to calculate the kb values from GiB and Tb
     # **********************************************
     def getDimensionedValue(value)
-             @ltmp     = Builtins.regexptokenize(value, "(.*)([G|T])")
+	    @ltmp     = Builtins.regexptokenize(value, "(\d+)(.*)$")
 	     size      = Ops.get_string(@ltmp, 0, "")
 	     dimension = Ops.get_string(@ltmp, 1, "")
 	     case dimension
-	       when "G"
+	       when "MiB"
+                  size = size.to_i*1024*1024
+	       when "M"
+                  size = size.to_i*1000*1000
+	       when "GiB"
                   size = size.to_i*1024*1024*1024
-	       when "T"
+	       when "G"
+                  size = size.to_i*1000*1000*1000
+	       when "TiB"
                   size = size.to_i*1024*1024*1024*1024
+	       when "T"
+                  size = size.to_i*1000*1000*1000*1000
                else
                   size = 1024*1024*1024
 	     end
