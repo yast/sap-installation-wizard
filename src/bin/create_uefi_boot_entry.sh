0be86572aff7b6f6ddf7a1854c614061bf0dcfa0
Peter Varkoly <varkoly@suse.com>
2020-08-30 17:32:42 +0200

    Make script safer.

--- src/bin/create_uefi_boot_entry.sh
+++ src/bin/create_uefi_boot_entry.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 if [ -d /sys/firmware/efi ]; then
-	DEVICE=$( sed -r 's/.*root=UUID=(\S+).*/\1/' /proc/cmdline )
-	efibootmgr -c -d /dev/disk/by-uuid/$DEVICE -p 2 -L "sapb1hana" -l \\EFI\\sles\\grubx64.efi
+	PUUID=$( sed -r 's/.*root=UUID=(\S+).*/\1/' /proc/cmdline )
+	PART=$( readlink -f /dev/disk/by-uuid/${PUUID} )
+	DEVICE=${PART/p[0-9]/}
+	if [ "${DEVICE}" == "${PART}" ]; then
+	    DEVICE=${PART/[0-9]/}
+	fi
+	efibootmgr -c -d $DEVICE -p 2 -L "sapb1hana" -l \\EFI\\BOOT\\GRUBX64.EFI
 fi
